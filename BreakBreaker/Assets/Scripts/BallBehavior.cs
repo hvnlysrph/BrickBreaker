7078c88e4bca2d8cb0c06706c03971798f15d05e
Bridget Cloud <bridgetcloud@ymail.com>
2020-01-21 20:40:32 -0600

    Initial File Upload

---
+++ BreakBreaker/Assets/Scripts/BallBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehavior : MonoBehaviour
+{
+    public float initialVelocity = 450f;
+
+    public GameManager manager;
+
+    Rigidbody rb;
+    bool ballInPlay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        manager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && ballInPlay == false)
+        {
+            transform.parent = null;
+            ballInPlay = true;
+            manager.inPlay = true;
+            rb.isKinematic = false;
+            rb.AddForce(new Vector3(initialVelocity, initialVelocity, 0));
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
